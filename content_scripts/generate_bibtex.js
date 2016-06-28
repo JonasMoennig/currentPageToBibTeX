a7503b56a5af33aab33c3341d6eb7d0218d18dcc
Jonas Mönnig <jonas.moennig@stud.tu-darmstadt.de>
2016-06-28 20:27:31 +0200

    implement basic author extraction (to be extended)

--- content_scripts/generate_bibtex.js
+++ content_scripts/generate_bibtex.js
@@ -1,12 +1,71 @@
+function getAuthor(){
+
+  var selectorsInnerHTML = ['[itemprop=author]'];
+  var selectorsAttribute = [['meta[name=author]', 'content']];
+  var result = [];
+    
+  selectorsInnerHTML.forEach(function(currentValue, index, array){
+    var tag = document.querySelector(currentValue);
+    if(tag != null){
+      result.push(tag.innerHTML);
+    }
+  });
+
+  selectorsAttribute.forEach(function(currentValue, index, array){
+    var tag = document.querySelector(currentValue[0]);
+    if(tag != null){
+      result.push(tag.getAttribute(currentValue[1]));
+    }
+  });
+
+  return result;
+
+}
+
+function getTitle(){
+
+  return [document.title];
+
+}
+
+function getYear(){
+
+  return document.querySelector('[itemprop=datePublished]').innerHTML.match(/(\D|^)\d\d\d\d(\D|$)/g);
+
+}
+
+function getURL(){
+
+  return [window.location]
+
+}
+
+function getURLDate(){
+
+  var now = new Date();
+  var mon = now.getMonth()+1;
+
+  return [now.getFullYear() + "-" + mon + "-" + now.getDate()];
+
+}
+
 function bibtex(request, sender, sendResponse){
   
-  var bibtex = "@online{cite_key, \n" +
-    "author = {$author$},\n" + 
-    "title = {$title$},\n" + 
-    "year = $year$,\n" + 
-    "url = {$url$},\n" +
-    "urldate = {$urldate$}\n" +
+  var bibtex = "@online{cite_key, <br>" +
+    "$indent$author = {$author$},<br>" + 
+    "$indent$title = {$title$},<br>" + 
+    "$indent$year = $year$,<br>" + 
+    "$indent$url = {$url$},<br>" +
+    "$indent$urldate = {$urldate$}<br>" +
     "}";
+
+  bibtex = bibtex.replace("$author$", getAuthor()[0]);
+  bibtex = bibtex.replace("$title$", getTitle()[0]);
+  bibtex = bibtex.replace("$year$", getYear()[0]);
+  bibtex = bibtex.replace("$url$", getURL()[0]);
+  bibtex = bibtex.replace("$urldate$", getURLDate()[0]);
+
+  bibtex = bibtex.replace(/\$indent\$/g, "&nbsp;&nbsp");
 
   sendResponse(bibtex);
   chrome.runtime.onMessage.removeListener(bibtex);
